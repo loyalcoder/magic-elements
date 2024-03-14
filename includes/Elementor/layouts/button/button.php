89fcfdf654888e3612a708b363eb02f9906ba9e8
rajendro00 <rajenkundu0000@gmail.com>
2024-03-14 16:55:07 +0600

    update

--- includes/Elementor/layouts/button/button.php
+++ includes/Elementor/layouts/button/button.php
@@ -5,7 +5,7 @@
               <?php \Elementor\Icons_Manager::render_icon( $settings['selected_icon'], [ 'aria-hidden' => 'true' ] );?>
             </span> 
             <span <?php echo $this->get_render_attribute_string( 'text'); ?>>
-             <?php echo esc_html($settings['text']) ?>
+              <?php echo esc_html($settings['text']) ?>
             </span>
         </span>
 	</a>
