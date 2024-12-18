2032e02e13d36ce11b83c1e98d6c46ed026e57a1
Md Nayeem Farid <39758380+nayeem07007@users.noreply.github.com>
2024-12-18 23:28:03 +0600

    update team

--- includes/Elementor/layouts/team.php
+++ includes/Elementor/layouts/team.php
@@ -5,7 +5,20 @@
             $this->add_render_attribute( 'team_image', 'alt', \Elementor\Control_Media::get_image_alt( $settings['team_image'] ) );
             $this->add_render_attribute( 'team_image', 'title', \Elementor\Control_Media::get_image_title( $settings['team_image'] ) );
             $this->add_render_attribute( 'team_image', 'class', '' );
-            echo \Elementor\Group_Control_Image_Size::get_attachment_image_html( $settings, 'thumbnail', 'team_image' );
+            echo wp_kses(
+                \Elementor\Group_Control_Image_Size::get_attachment_image_html( $settings, 'thumbnail', 'team_image' ), 
+                array(
+                    'img' => array(
+                        'src'    => true,
+                        'alt'    => true,
+                        'width'  => true,
+                        'height' => true,
+                        'class'  => true,
+                        'srcset' => true,
+                        'sizes'  => true,
+                    ),
+                )
+            );
         ?>
     </div>
     <h2><?php echo esc_html($settings['team_title']); ?></h2>
@@ -17,18 +30,14 @@
             ?>
                 <?php if(!empty($team_icon_list)) {?>
                     <?php foreach($team_icon_list as $list) { 
-                        $icon_link = (!empty($list['icon_link']['url']) ? $list['icon_link']['url'] : '' );
+                        if ( ! empty( $list['icon_link']['url'] ) ) {
+                            $this->add_link_attributes( 'icon_link'.$list['_id'], $list['icon_link'] );
+                        }
                         ?>
                         <div class="social-icon">
-                            <?php if(  $icon_link ) { ?>
-                                <a href="<?php echo esc_url( $icon_link); ?>">
-                                    <?php \Elementor\Icons_Manager::render_icon( $list['team_social_icon'], [ 'aria-hidden' => 'true' ] ); ?>
-                                </a>
-                           <?php }else{ ?>
-                            <div>
-                                <?php \Elementor\Icons_Manager::render_icon( $list['team_social_icon'], [ 'aria-hidden' => 'true' ] ); ?> 
-                            </div>
-                          <?php } ?>
+                            <a <?php $this->print_render_attribute_string( 'icon_link'.$list['_id'] ); ?>>
+                                <?php \Elementor\Icons_Manager::render_icon( $list['team_social_icon'], [ 'aria-hidden' => 'true' ] ); ?>
+                            </a>
                         </div>
                     <?php } } ?>
                 <?
