87c256c0d99c4e605eee455e7428c8de60e4ef1c
rajendro00 <rajenkundu0000@gmail.com>
2024-12-20 16:30:49 +0600

    update

--- includes/Elementor.php
+++ includes/Elementor.php
@@ -133,6 +133,7 @@ class Load_Elementor
             'emkit-image-accordion' => [
                 'src'     => EM_KIT_ASSETS . '/dist/image_accordion.css',
                 'version' => filemtime(EM_KIT_PATH . '/assets/dist/image_accordion.css'),
+            ],
             'emkit-flipcard' => [
                 'src'     => EM_KIT_ASSETS . '/dist/flipcard.css',
                 'version' => filemtime(EM_KIT_PATH . '/assets/dist/flipcard.css'),
