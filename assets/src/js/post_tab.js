6f088aa02edfd4b8adf25c4e27f32275026c3072
rajendro00 <rajenkundu0000@gmail.com>
2024-12-20 19:33:52 +0600

    update

--- assets/src/js/post_tab.js
+++ assets/src/js/post_tab.js
@@ -7,7 +7,7 @@ import "./../scss/post_tab.scss";
       onInit: function () {
         var E_FRONT = elementorFrontend;
         var widgetHandlersMap = {
-          "em_kit_button.default": emkElementor.EmKitButton,
+          "em_kit_post_tab.default": emkElementor.EmKitPostTab,
         };
   
         $.each(widgetHandlersMap, function (widgetName, callback) {
@@ -15,8 +15,31 @@ import "./../scss/post_tab.scss";
         });
       },
 
-      EmKitButton: function ($scope) {
-         alert();
+      EmKitPostTab: function ($scope) {
+        jQuery(document).ready(function ($) {
+          // Initially show all posts
+          $('.post-item').show();
+      
+          // Tab click event
+          $('.nav-link').on('click', function () {
+              // Remove active class from all tabs
+              $('.nav-link').removeClass('active');
+              // Add active class to clicked tab
+              $(this).addClass('active');
+      
+              // Get the category ID from the clicked tab
+              const category = $(this).data('category');
+      
+              // Show/hide posts based on category
+              if (category === 'all') {
+                  $('.post-item').show(); // Show all posts
+              } else {
+                  $('.post-item').hide(); // Hide all posts
+                  $('.post-item.category-' + category).show(); // Show posts with selected category
+              }
+          });
+      });
+      
       },
     };
   
