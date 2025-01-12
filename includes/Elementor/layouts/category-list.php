89905429c1221d4674ae94aa2a8a718df43f70d8
Md Nayeem Farid <39758380+nayeem07007@users.noreply.github.com>
2025-01-12 20:41:49 +0600

    Update

--- includes/Elementor/layouts/category-list.php
+++ includes/Elementor/layouts/category-list.php
@@ -1,22 +1,38 @@
-<?php
-  // Render the List
-  if ( ! empty( $settings['category_items'] ) ) {
-    echo '<ul class="category-list-widget" style="display: flex; flex-direction: ' . esc_attr( $flex_direction ) . '; ' . esc_attr( $align_property ) . ': ' . esc_attr( $alignment ) . ';">';
-    foreach ( $settings['category_items'] as $item ) {
-        $category_id = $item['selected_category'];
-        $icon = $item['icon']['value'];
+<?php 
+    if ( ! empty( $settings['category_items'] ) ) {
+        // Initialize an empty variable to store the HTML
+        $output = '<ul class="category-list-widget" style="display: flex; flex-direction: ' . esc_attr( $flex_direction ) . '; ' . esc_attr( $align_property ) . ': ' . esc_attr( $alignment ) . ';">';
 
-        if ( $category_id ) {
-            $category = get_category( $category_id );
-            if ( $category ) {
-                echo '<li>';
-                echo '<i class="' . esc_attr( $icon ) . '"></i> ';
-                echo '<a href="' . esc_url( get_category_link( $category_id ) ) . '">';
-                echo esc_html( $category->name );
-                echo '</a>';
-                echo '</li>';
+        foreach ( $settings['category_items'] as $item ) {
+            $category_id = $item['selected_category'];
+            $icon = $item['icon']['value'];
+
+            if ( $category_id ) {
+                $category = get_category( $category_id );
+                if ( $category ) {
+                    $output .= '<li>';
+                    $output .= '<i class="' . esc_attr( $icon ) . '"></i> ';
+                    $output .= '<a href="' . esc_url( get_category_link( $category_id ) ) . '">';
+                    $output .= esc_html( $category->name );
+                    $output .= '</a>';
+                    $output .= '</li>';
+                }
             }
         }
+
+        $output .= '</ul>';
+
+        // Echo the sanitized output using wp_kses
+        echo wp_kses(
+            $output,
+            array(
+                'ul' => array(
+                    'class' => array(),
+                    'style' => array(),
+                ),
+                'li' => array(),
+                'i'  => array( 'class' => array() ),
+                'a'  => array( 'href' => array() ),
+            )
+        );
     }
-    echo '</ul>';
-}
