7300b238ce30eece0584a3ac0da5feddf6cff354
Akash <103049247+mdakash001@users.noreply.github.com>
2025-05-04 18:51:35 +0600

    done

--- includes/Elementor/layouts/Data-Table/data-table.php
+++ includes/Elementor/layouts/Data-Table/data-table.php
@@ -1,33 +1,33 @@
 <?php 
- if (empty($columns) || empty($rows)) {
-    return;
-}
+    if (empty($columns) || empty($rows)) {
+        return;
+    }
 ?>
-   <div class="elementor-data-table">
-            <table>
-                <thead>
-                    <tr>
-                        <?php foreach ($columns as $column): ?>
-                            <th style="width: <?php echo esc_attr($column['column_width']['size'] . $column['column_width']['unit']); ?>;">
-                                <?php echo esc_html($column['column_name']); ?>
-                            </th>
-                        <?php endforeach; ?>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($rows as $row): ?>
-                        <tr>
-                            <?php 
-                            $cells = $row['row_cells'];
-                            for ($i = 0; $i < count($columns); $i++): 
-                                $cell_content = isset($cells[$i]) ? $cells[$i]['cell_content'] : '';
-                            ?>
-                                <td>
-                                    <?php echo wp_kses_post($cell_content); ?>
-                                </td>
-                            <?php endfor; ?>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
+<div class="elementor-data-table">
+    <table>
+        <thead>
+            <tr>
+                <?php foreach ($columns as $column): ?>
+                    <th style="width: <?php echo esc_attr($column['column_width']['size'] . $column['column_width']['unit']); ?>;">
+                        <?php echo esc_html($column['column_name']); ?>
+                    </th>
+                <?php endforeach; ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($rows as $row): ?>
+                <tr>
+                    <?php 
+                    $cells = $row['row_cells'];
+                    for ($i = 0; $i < count($columns); $i++): 
+                        $cell_content = isset($cells[$i]) ? $cells[$i]['cell_content'] : '';
+                    ?>
+                        <td>
+                            <?php echo wp_kses_post($cell_content); ?>
+                        </td>
+                    <?php endfor; ?>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
