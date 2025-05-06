2c7d7b35b4e94fb72ec83962afba7232d478eb70
Akash <103049247+mdakash001@users.noreply.github.com>
2025-05-06 16:37:11 +0600

    done

---
+++ assets/src/js/light_box.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($) {
+    // Lightbox trigger
+    $(document).on('click', '.lightbox-trigger', function(e) {
+        e.preventDefault();
+        var target = $(this).data('target');
+        $(target).fadeIn();
+        $('body').addClass('lightbox-open');
+    });
+    // Close lightbox
+    $(document).on('click', '.lightbox-close, .custom-lightbox-content', function(e) {
+        if ($(e.target).hasClass('custom-lightbox-content') || $(e.target).hasClass('lightbox-close')) {
+            $('.custom-lightbox-content').fadeOut();
+            $('body').removeClass('lightbox-open');
+        }
+    });
+    // Close with ESC key
+    $(document).keyup(function(e) {
+        if (e.key === "Escape") {
+            $('.custom-lightbox-content').fadeOut();
+            $('body').removeClass('lightbox-open');
+        }
+    });
+});
--- includes/Elementor/Light_Box.php
+++ includes/Elementor/Light_Box.php
@@ -114,15 +114,120 @@ class Light_Box extends Widget_Base
      */
     protected function register_light_box_controls()
     {
-    // Content Section
-    $this->start_controls_section(
-        "section_content",
-        [
-            "label" => __("Lightbox Content", "custom-elementor-widgets"),
-        ]
-    );
+  // Content Tab
+  $this->start_controls_section(
+    'content_section',
+    [
+        'label' => __('Content', 'my-elementor-widgets'),
+        'tab' => Controls_Manager::TAB_CONTENT,
+    ]
+);
 
-    $this->end_controls_section();
+$this->add_control(
+    'lightbox_type',
+    [
+        'label' => __('Lightbox Content Type', 'my-elementor-widgets'),
+        'type' => Controls_Manager::SELECT,
+        'default' => 'image',
+        'options' => [
+            'image' => __('Image', 'my-elementor-widgets'),
+            'text' => __('Text', 'my-elementor-widgets'),
+            'video' => __('Video', 'my-elementor-widgets'),
+        ],
+    ]
+);
+
+$this->add_control(
+    'image_content',
+    [
+        'label' => __('Image', 'my-elementor-widgets'),
+        'type' => Controls_Manager::MEDIA,
+        'default' => [
+            'url' => \Elementor\Utils::get_placeholder_image_src(),
+        ],
+        'condition' => [
+            'lightbox_type' => 'image',
+        ],
+    ]
+);
+
+$this->add_control(
+    'text_content',
+    [
+        'label' => __('Text Content', 'my-elementor-widgets'),
+        'type' => Controls_Manager::WYSIWYG,
+        'default' => __('This is your lightbox text content', 'my-elementor-widgets'),
+        'condition' => [
+            'lightbox_type' => 'text',
+        ],
+    ]
+);
+
+$this->add_control(
+    'video_content',
+    [
+        'label' => __('Video URL', 'my-elementor-widgets'),
+        'type' => Controls_Manager::TEXT,
+        'placeholder' => __('Enter YouTube or Vimeo URL', 'my-elementor-widgets'),
+        'condition' => [
+            'lightbox_type' => 'video',
+        ],
+    ]
+);
+
+$this->add_control(
+    'trigger_type',
+    [
+        'label' => __('Trigger Type', 'my-elementor-widgets'),
+        'type' => Controls_Manager::SELECT,
+        'default' => 'button',
+        'options' => [
+            'button' => __('Button', 'my-elementor-widgets'),
+            'image' => __('Image', 'my-elementor-widgets'),
+            'text' => __('Text Link', 'my-elementor-widgets'),
+        ],
+    ]
+);
+
+$this->add_control(
+    'button_text',
+    [
+        'label' => __('Button Text', 'my-elementor-widgets'),
+        'type' => Controls_Manager::TEXT,
+        'default' => __('Open Lightbox', 'my-elementor-widgets'),
+        'condition' => [
+            'trigger_type' => 'button',
+        ],
+    ]
+);
+
+$this->add_control(
+    'trigger_image',
+    [
+        'label' => __('Trigger Image', 'my-elementor-widgets'),
+        'type' => Controls_Manager::MEDIA,
+        'default' => [
+            'url' => \Elementor\Utils::get_placeholder_image_src(),
+        ],
+        'condition' => [
+            'trigger_type' => 'image',
+        ],
+    ]
+);
+
+$this->add_control(
+    'trigger_text',
+    [
+        'label' => __('Trigger Text', 'my-elementor-widgets'),
+        'type' => Controls_Manager::TEXT,
+        'default' => __('Click to open', 'my-elementor-widgets'),
+        'condition' => [
+            'trigger_type' => 'text',
+        ],
+    ]
+);
+
+$this->end_controls_section();
 
             // Style section
         $this->start_controls_section(
@@ -149,6 +254,7 @@ class Light_Box extends Widget_Base
     protected function render()
     {
         $settings = $this->get_settings_for_display();
+        $unique_id = 'lightbox-' . $this->get_id();
 
 
         include __DIR__ . '/layouts/Light-Box/light-box.php';
