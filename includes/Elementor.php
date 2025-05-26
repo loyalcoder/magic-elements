0267148903aa0511fc10ea186d6ca22f3201f2e8
rajendro00 <rajenkundu0000@gmail.com>
2025-05-26 20:22:05 +0600

    update

--- includes/Elementor.php
+++ includes/Elementor.php
@@ -158,6 +158,11 @@ class Load_Elementor
                 'version' => filemtime(MAGIC_ELEMENTS_PATH . '/assets/dist/image_compare.js'),
                 'deps'    => ['jquery']
             ],
+            'emkit-timeline' => [
+                'src'     => MAGIC_ELEMENTS_ASSETS . '/dist/timeline.js',
+                'version' => filemtime(MAGIC_ELEMENTS_PATH . '/assets/dist/timeline.js'),
+                'deps'    => ['jquery']
+            ],
         ];
     }
     /**
@@ -246,6 +251,10 @@ class Load_Elementor
                 'src'     => MAGIC_ELEMENTS_ASSETS . '/dist/image_compare.css',
                 'version' => filemtime(MAGIC_ELEMENTS_PATH . '/assets/dist/image_compare.css'),
             ],
+            'emk-timeline' => [
+                'src'     => MAGIC_ELEMENTS_ASSETS . '/dist/timeline.css',
+                'version' => filemtime(MAGIC_ELEMENTS_PATH . '/assets/dist/timeline.css'),
+            ],
         ];
     }
 
@@ -282,6 +291,7 @@ class Load_Elementor
             'Info_Box',
             'Feature_list',
             'Review',
+            'Timeline',
         ];
     }
     public static function defaultWidgets()
@@ -310,6 +320,7 @@ class Load_Elementor
             'infobox',
             'featurelist',
             'review',
+            'timeline',
         ];
     }
     /**
